29969bb44333f478bae73eb8c5a18402f880ccf0
aamirams93 <aamirams93@gmail.com>
2024-08-07 13:08:17 +0530

    first commit

---
+++ bookapp/src/app/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+    bookId:number;
+    bookName:string;
+    bookPrice:number;
+
+     constructor(a:number,b:string,c:number){
+        this.bookId = a;
+        this.bookName = b;
+        this.bookPrice = c;
+    }
+}
